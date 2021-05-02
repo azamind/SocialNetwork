6855f6721e90d93a5d867cf4ed989166651c010e
azamind <azamind@gmail.com>
2021-05-02 13:31:20 +0300

    a bunch of child and parent component register and home

--- Client/src/app/_services/account.service.ts
+++ Client/src/app/_services/account.service.ts
@@ -24,12 +24,25 @@ export class AccountService {
           localStorage.setItem('user', JSON.stringify(user));
           this.currentUserSource.next(user);
         }
+        return user;
       })
     );
   }
 
   setCurrentUser(user: User) {
     this.currentUserSource.next(user);
+  }
+
+  register(model: any) {
+    return this.http.post<User>(this.baseUrl + 'account/register', model).pipe(
+      map((user: User) => {
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+        return user;
+      })
+    );
   }
 
   logout() {
