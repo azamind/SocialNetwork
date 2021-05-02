b110801ccd8384381f464c06fb7c38eb65d26a0e
azamind <azamind@gmail.com>
2021-05-02 22:31:10 +0300

    toastr notify include

--- Client/src/app/nav/nav.component.ts
+++ Client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { AccountService } from './../_services/account.service';
 
 @Component({
@@ -9,21 +11,27 @@ import { AccountService } from './../_services/account.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(public accountService: AccountService) { }
+  constructor(
+    public accountService: AccountService,
+    private router: Router,
+    private toastrService: ToastrService
+  ) { }
 
   ngOnInit(): void {
   }
 
   login() {
     return this.accountService.login(this.model).subscribe(response => {
-      console.log(response);
+      this.router.navigateByUrl('/members');
     }, error => {
         console.log(error);
+        this.toastrService.error(error.error);
     });
   }
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 
 }
