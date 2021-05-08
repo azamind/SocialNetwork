1a3e32afa78d10c0602636c2ad19dd65d6ea6692
azamind <azamind@gmail.com>
2021-05-08 22:38:21 +0300

    add new fields for user, create table photos and generate new migration

--- Entities/AppUser.cs
+++ Entities/AppUser.cs
@@ -1,16 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using SocialNetworkProject.Extensions;
 
 namespace SocialNetworkProject.Entities
 {
     public class AppUser
     {
         public int Id { get; set; }
-
         public string UserName { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+        public DateTime DateOfBirth { get; set; } = DateTime.Now;
+        public DateTime LastActive { get; set; } = DateTime.Now;
+        public string Gender { get; set; }
+        public string Introduction { get; set; }
+        public string Interest { get; set; }
+        public string City { get; set; }
+        public string Country{ get; set; }
+        public ICollection<Photo> Photos { get; set; }
+
+        public int GeAge()
+        {
+            return DateOfBirth.CalculateAge();
+        }
     }
 }
