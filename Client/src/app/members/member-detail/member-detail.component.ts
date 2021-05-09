4b620785d6516bb42d45142e2da00578f8b61555
azamind <azamind@gmail.com>
2021-05-09 19:34:40 +0300

    routing detail page

--- Client/src/app/members/member-detail/member-detail.component.ts
+++ Client/src/app/members/member-detail/member-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Member } from '../../_modules/member';
+import { MembersService } from '../../_services/members.service';
 
 @Component({
   selector: 'app-member-detail',
@@ -6,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./member-detail.component.css']
 })
 export class MemberDetailComponent implements OnInit {
+  member!: Member;
 
-  constructor() { }
+  constructor(private memberService: MembersService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.loadMember();
+  }
+
+  loadMember() {
+    var specificUser: string|null = this.route.snapshot.paramMap.get("username");
+    this.memberService.getMember(specificUser).subscribe(member => {
+      this.member = member;
+    });
   }
 
 }
