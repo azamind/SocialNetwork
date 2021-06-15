d579c7caab7a2a1d436deedc2cd115648296e793
azamind <azamind@gmail.com>
2021-06-15 17:43:33 +0300

    frontend sorting memebers

--- Client/src/app/_models/userParams.ts
+++ Client/src/app/_models/userParams.ts
@@ -6,6 +6,7 @@ export class UserParams {
   maxAge = 150;
   pageNumber = 1;
   pageSize = 5;
+  orderBy = 'lastActive';
 
   constructor(user: User | any) {
     this.gender = user.gender === 'female' ? 'male' : 'female';
